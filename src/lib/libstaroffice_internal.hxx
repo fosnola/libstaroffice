a90326d10f1f63e78b996fd3db1cb4c43960e406
osnola <alonso@loria.fr>
2017-07-19 12:04:41 +0200

    clang: do not use defined(__has_cpp_attribute) and __has_cpp_attribute(clang::fallthrough) on the same line...

--- src/lib/libstaroffice_internal.hxx
+++ src/lib/libstaroffice_internal.hxx
@@ -113,9 +113,11 @@ struct STOFF_shared_ptr_noop_deleter {
 /** fall through attributes */
 #define STOFF_FALLTHROUGH
 #if defined(__clang__)
-#  if defined(__has_cpp_attribute) && __has_cpp_attribute(clang::fallthrough)
-#    undef STOFF_FALLTHROUGH
-#    define STOFF_FALLTHROUGH [[clang::fallthrough]]
+#  if defined(__has_cpp_attribute)
+#    if __has_cpp_attribute(clang::fallthrough)
+#      undef STOFF_FALLTHROUGH
+#      define STOFF_FALLTHROUGH [[clang::fallthrough]]
+#    endif
 #  endif
 #elif defined(__GNUC__)
 #  if __GNUC__>=7
